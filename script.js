59fed0d64bf81dacd6fe59e24ec3577fb35cc56a
Phil Taylor <phil.c.taylor@gmail.com>
2023-01-17 23:04:08 -0500

    user now triggers grid with Enter and background color changes on hover

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+/*
+// When a user hovers on a cell, turn it to a color
+function hoverColor(elemClass) {
+    let cells = document.querySelectorAll('.cell')
+    for (let i = 0; i < elems.length; i++) {
+        cells[i].addEventListener('mousover', function() {
+            this.style.backgroundColor = "red";
+        })
+    }
+} */
+
 // Create an arbitrarily large grid as the display canvas
 const canvas = document.getElementById("canvas");
 function createGrid(axisLength) {
@@ -16,9 +27,23 @@ function createGrid(axisLength) {
     }
     canvas.style.setProperty("grid-template-columns", `repeat(${axisLength}, 1fr)`);
     canvas.style.setProperty("grid-template-rows", `repeat(${axisLength}, 1fr)`);
-}
+
+    // when someone hovers on a cell, it should change color
+    let cells = document.querySelectorAll('.cell');
+    cells.forEach(function(cell) {
+        cell.addEventListener('mouseover', function() {
+            cell.style.backgroundColor = "red";
+        });
+    });
+};
 
 
 // Let users dictate the size of the canvas
 let inputField = document.getElementById("input-length");
-inputField.addEventListener("keydown", (event) => createGrid(event.key));
+inputField.addEventListener("keydown", function(event) {
+    if (event.key == 'Enter') {
+        canvas.innerHTML = "";
+        createGrid(inputField.value);
+    }
+});
+
